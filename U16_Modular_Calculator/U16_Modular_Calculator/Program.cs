b481df6d9bcd908f4130629a7107409b61162909
saeed-c <55282225+saeed-c@users.noreply.github.com>
2020-01-10 15:37:58 +0000

    Add files via upload

---
+++ U16_Modular_Calculator/U16_Modular_Calculator/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U16_Modular_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator MyCalc = new Calculator();
+
+            Console.WriteLine("##################################################################");
+            Console.WriteLine("###############    C# C A L C U L A T O R   ######################");
+            Console.WriteLine("##################################################################");
+
+            bool calcloop = true;
+            while (calcloop == true)
+            {
+                Console.Write("\nEnter your first number: ");
+                double inputOne = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter your second number; ");
+                double inputTwo = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("\nMenu");
+                Console.WriteLine("Enter 1 for addition");
+                Console.WriteLine("Enter 2 for Subtraction");
+                Console.WriteLine("Enter 3 for Multiplication");
+                Console.WriteLine("Enter 4 for Division");
+                Console.WriteLine("");
+
+                int opChoice = Convert.ToInt32(Console.ReadLine());
+                switch (opChoice)
+                {
+                    case 1:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Add(inputOne, inputTwo).ToString());
+                        break;
+                    case 2:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Subtract(inputOne, inputTwo).ToString());
+                        break;
+                    case 3:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Multiply(inputOne, inputTwo).ToString());
+                        break;
+                    case 4:
+                        Console.WriteLine("\nYour answer is: " + MyCalc.Divide(inputOne, inputTwo).ToString());
+                        break;
+                    default:
+                        break;       
+                }
+
+                Console.WriteLine("");
+                Console.Write("\nDo you want to try another sum? Y/N: ");
+                string restartChoice = Console.ReadLine().ToUpper();
+
+                if (restartChoice == "Y")
+                {
+                    Console.WriteLine("Press any key to continue");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Press any key to exit");
+                    calcloop = false;
+
+                }
+
+                Console.ReadKey();
+
+            }
+
+
+        }
+    }
+}
---
+++ U16_Modular_Calculator/U16_Modular_Calculator/calculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace U16_Modular_Calculator
+{
+    class Calculator
+    {
+        //This is a calculator class and below are the member functions:
+
+        //Addition function
+        public double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+        
+        //Subtraction function
+        //Basic validation added to ensure that we subtract the smaller number from larger number
+        public double Subtract(double num1, double num2)
+        {
+            if (num1 > num2)
+            {
+                return num1 - num2;
+            }
+            return num2 - num1;
+        }
+
+        //Multiplication function 
+        public double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        //Division function
+        public double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+   
+    }
+}
